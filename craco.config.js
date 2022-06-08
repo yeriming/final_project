b2ad968bcbdbd278e3ff496dca827b0325f7c626
yeriming <100740625+yeriming@users.noreply.github.com>
2022-06-08 10:24:18 +0900

    build

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,16 @@
+const path = require("path");
+
 module.exports = {
   style: {
     postcssOptions: {
-      plugins: [require('tailwindcss'), require('autoprefixer')],
+      plugins: [require("tailwindcss"), require("autoprefixer")],
+    },
+  },
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      paths.appBuild = webpackConfig.output.path =
+        path.resolve("docs");
+      return webpackConfig; // Important: return the modified config
     },
   },
 };
