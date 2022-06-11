34e6112b036e8808e08ef21488ff151ba84e4870
yeriming <100740625+yeriming@users.noreply.github.com>
2022-06-11 21:31:00 +0900

    messages

--- src/pages/DMPage.tsx
+++ src/pages/DMPage.tsx
@@ -1,14 +1,30 @@
-import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { RouteComponentProps, useHistory, useParams } from 'react-router-dom';
 
-export const DMPage = () => {
+interface MatchParams {
+  id: string;
+}
+
+export function DMPage({ match }: RouteComponentProps<MatchParams>) {
   const { push } = useHistory();
+  const [messages, setMessages] = useState([]);
+  const { id } = match.params;
+
+  useEffect(() => {
+    const roomsData = axios
+      .get(`http://localhost:1337/api/rooms/${id}?populate=*`)
+      .then((res) => {
+        setMessages(res.data.data.attributes.messages.data);
+        // console.log(res.data.data.attributes.messages.data);
+      });
+  }, []);
+
   return (
-<div>
- <div>aa</div>
-
-
-
-</div>
-
-  )
+    <div>
+      {messages.map((msg: any) => {
+        return <h1>{msg.attributes.content}</h1>;
+      })}
+    </div>
+  );
 }
