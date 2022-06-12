0ab5adc862ae7e4352f3901fd167634c40ff9fd6
yeriming <100740625+yeriming@users.noreply.github.com>
2022-06-12 17:05:59 +0900

    dm

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -40,39 +40,7 @@ export const ChatPage = () => {
             </div>
           );
         })}
-        {/* 
-        <div className="flex m-10 bg-teal-100 rounded-xl">
-          <img
-            src="https://image.made-in-china.com/44f3j00hmVHAvIcAtkY/Fashion-Design-Unique-Kids-Formal-Dress-Frock-Evening-Wedding-Party-Child-Clothes-Girl-Dress.jpg"
-            alt=""
-            className="w-20 h-20 rounded-full m-5"
-          />
-          <div className="text-2xl text-teal-900 font-semibold ml-5 mt-10">
-            W's Design
-          </div>
-        </div>
-
-        <div className="flex m-10 bg-teal-100 rounded-xl">
-          <img
-            src="https://assets.vogue.in/photos/5e170d089929c60008a66195/2:3/w_2560%2Cc_limit/79750875_814222359027955_4976547311530923983_n.jpg"
-            alt=""
-            className="w-20 h-20 rounded-full m-5"
-          />
-          <div className="text-2xl text-teal-900 font-semibold ml-5 mt-10">
-            H's Design
-          </div>
-        </div>
-
-        <div className="flex m-10 bg-teal-100 rounded-xl">
-          <img
-            src="https://ae01.alicdn.com/kf/HTB1JtxDIXXXXXbXXXXXq6xXFXXXt/vintage-dresses-unique-club-wear-1940s-online-boutique-cotton-linen-mod-clothes-retro-hippie-unique-rock.jpg"
-            alt=""
-            className="w-20 h-20 rounded-full m-5"
-          />
-          <div className="text-2xl text-teal-900 font-semibold ml-5 mt-10">
-            A's Design
-          </div>
-        </div> */}
+        
       </div>
 
       <div className="relative">
--- src/pages/DMPage.tsx
+++ src/pages/DMPage.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { RouteComponentProps, useHistory, useParams } from 'react-router-dom';
+import { TextField } from '../components/TextField';
 
 interface MatchParams {
   id: string;
@@ -10,21 +11,55 @@ export function DMPage({ match }: RouteComponentProps<MatchParams>) {
   const { push } = useHistory();
   const [messages, setMessages] = useState([]);
   const { id } = match.params;
+  const [roomTitle, setRoomTitle] = useState('');
+ 
+
 
   useEffect(() => {
     const roomsData = axios
       .get(`http://localhost:1337/api/rooms/${id}?populate=*`)
       .then((res) => {
         setMessages(res.data.data.attributes.messages.data);
-        // console.log(res.data.data.attributes.messages.data);
+        const title = res.data.data.attributes.title;
+        setRoomTitle(title);
       });
   }, []);
 
   return (
     <div>
-      {messages.map((msg: any) => {
-        return <h1>{msg.attributes.content}</h1>;
-      })}
+      <div>
+        <div className="text-2xl text-teal-900 font-semibold ml-5 mt-10 text-center">
+          {roomTitle}
+        </div>
+      </div>
+
+      <div>
+        <div>
+          {messages.map((msg: any) => {
+            return (
+              <div key={msg.id}>
+                <div className="text-xl text-teal-900 m-5 bg-teal-100 rounded-l p-3">
+                  <h1>{msg.attributes.content}</h1>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      
+        <div className="relative">
+          <TextField
+            placeholder="입력하세요."
+            className="fixed bottom-0 left-0 right-0"
+          />
+          <a
+          href=""
+          className="fixed bottom-0 right-0 bg-teal-500 font-bold text-white px-4 py-4 transition duration-300 ease-in-out hover:bg-teal-600 mr-6 rounded-md">
+          
+          send
+          </a>
+        </div>
+        
+      </div>
     </div>
   );
 }
